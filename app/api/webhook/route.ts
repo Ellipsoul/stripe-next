4b651c1a9f2d55c2bab28a92802fece9ef4bec90
Aron Teh <aronteh.chess@gmail.com>
2024-04-21 12:05:02 +0100

    Attempt to diagnose webhook issues

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,11 +6,9 @@ import Stripe from "stripe";
 // Listen to webhook events from Stripe
 export async function POST(request: NextRequest) {
   try {
-    // Retrieve the event text and Stripe signature
     const rawBody = await request.text();
     const signature = request.headers.get("stripe-signature");
 
-    // Validate that the webhook came from stripe
     let event: Stripe.Event;
     try {
       event = stripe.webhooks.constructEvent(
@@ -18,57 +16,54 @@ export async function POST(request: NextRequest) {
         signature!,
         process.env.STRIPE_WEBHOOK_SECRET!
       );
-
-      // Handle successful checkout
-      if (event.type === "checkout.session.completed") {
-        const session: Stripe.Checkout.Session = event.data.object;
-        console.log(session);
-        // Retrieve the Supabase user_id from the metadata
-        const userId = session.metadata?.user_id;
-
-        // Create or update the stripe_customer_id in the stripe_customers table
-        const { error } = await supabaseAdmin.from("stripe_customers").upsert({
-          user_id: userId,
-          stripe_customer_id: session.customer,
-          subscription_id: session.subscription,
-          plan_active: true,
-          plan_expires: null,
-        });
-        if (error) console.error(`Error updating Supabase records: ${error}`);
-      }
-
-      // When a customer cancels their subscription, their plan remains active until the
-      // end of their billing cycle, and this webhook event is received
-      if (event.type === "customer.subscription.updated") {
-        const subscription: Stripe.Subscription = event.data.object;
-        console.log(subscription);
-        // Update the plan_expires field in the stripe_customers table
-        const { error } = await supabaseAdmin
-          .from("stripe_customers")
-          .update({ plan_expires: subscription.cancel_at })
-          .eq("subscription_id", subscription.id);
-        if (error) console.error(`Error: ${error}`);
-      }
-
-      // Stripe sends this event when a customer subscription plan ends
-      if (event.type === "customer.subscription.deleted") {
-        const subscription = event.data.object;
-        console.log(subscription);
-        // Update the plan_active field
-        const { error } = await supabaseAdmin
-          .from("stripe_customers")
-          .update({ plan_active: false, subscription_id: null })
-          .eq("subscription_id", subscription.id);
-        if (error) console.error(`Error: ${error}`);
-      }
-
-      return NextResponse.json({ message: "success" });
     } catch (error: any) {
       console.error(`Webhook signature verification failed: ${error.message}`);
-      return NextResponse.json({ message: "Webhook Error" }, { status: 400 });
+      return NextResponse.json(
+        { statusCode: 400, message: "Webhook Error" },
+        { status: 400 }
+      );
     }
 
-    //
+    // Handle the checkout.session.completed event
+    if (event.type === "checkout.session.completed") {
+      const session: Stripe.Checkout.Session = event.data.object;
+      console.log(session);
+      const userId = session.metadata?.user_id;
+
+      // Create or update the stripe_customer_id in the stripe_customers table
+      await supabaseAdmin.from("stripe_customers").upsert({
+        user_id: userId,
+        stripe_customer_id: session.customer,
+        subscription_id: session.subscription,
+        plan_active: true,
+        plan_expires: null,
+      });
+    }
+
+    // Update expires at time when user cancels subscription
+    // The subscription remains active until the end of the user's billing period
+    if (event.type === "customer.subscription.updated") {
+      const subscription: Stripe.Subscription = event.data.object;
+      console.log(subscription);
+      // Update the plan_expires field in the stripe_customers table
+      await supabaseAdmin
+        .from("stripe_customers")
+        .update({ plan_expires: subscription.cancel_at })
+        .eq("subscription_id", subscription.id);
+    }
+
+    // Sets the plan active status to false when the user subscription is no longer active
+    if (event.type === "customer.subscription.deleted") {
+      const subscription = event.data.object;
+      console.log(subscription);
+
+      await supabaseAdmin
+        .from("stripe_customers")
+        .update({ plan_active: false, subscription_id: null })
+        .eq("subscription_id", subscription.id);
+    }
+
+    return NextResponse.json({ statusCode: 200, message: "success" });
   } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 500 });
   }
